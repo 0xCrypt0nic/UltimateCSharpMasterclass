f86bfeabf1cc98a18ac82c967388621da210e486
0xCrypt0nic <n.greneche45@gmail.com>
2025-12-06 21:55:50 +0100

    ToDoList project

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello !");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+Console.WriteLine($"User input: {userChoice}");
