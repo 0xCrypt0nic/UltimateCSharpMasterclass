1a9ba9ff957f1f8ade472f311bd7477b0f0b489e
0xCrypt0nic <n.greneche45@gmail.com>
2025-12-07 22:23:12 +0100

    Calculator exercice finished

--- ToDoList/Calculator/Program.cs
+++ ToDoList/Calculator/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello !");
+
+// Input for two parameters of calculator
+
+Console.WriteLine("Input the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+// Ask type of calculation
+
+Console.WriteLine("What do you want to do with those numbers ?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubstract");
+Console.WriteLine("[M]ultiply");
+string choice = Console.ReadLine().ToUpper();
+
+// Calculation
+
+if (choice.Equals("A"))
+{
+    Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+}
+else if (choice.Equals("S"))
+{
+    Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+}
+else if (choice.Equals("M"))
+{
+    Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
+}
+else
+{
+    Console.WriteLine("Invalid option");
+}
+
+Console.WriteLine("Press any key to close");
+var close = Console.ReadLine();
+return;
