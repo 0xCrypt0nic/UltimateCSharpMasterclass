1a113bc5e98f09dcda55779f3705f4eee81d76b9
0xCrypt0nic <n.greneche45@gmail.com>
2025-12-07 21:06:00 +0100

    Methods part 1

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -7,40 +7,65 @@ Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 
 string userChoice = Console.ReadLine();
-Console.WriteLine($"User input: {userChoice}");
+
+// Console.WriteLine($"User input: {userChoice}");
 
 //todo: handle user's input.
 /*
 This is a multiple line comment.
 */
+// 
+// // Boolean and logical operation
+// bool someBoolean = true;
+// var someOtherBoolean = false;
+// bool isUserInputAbc = userChoice == "ABC";
+// bool isUserInputNotAbc = userChoice != "ABC";
+// bool isUserInputNotAbc2 = !(userChoice == "ABC");
+// 
+// var number = 10;
+// var isLargerThan5 = number > 5;
+// var isSmallerThan10 = number < 10;
+// var isLargerOrEqualTo10 = number >= 10;
+// var isSmallerOrEqualTo6 = number <= 6;
+// 
+// var is10Modulo3EqualTo1 = 10 % 3 == 1;
+// var isEven = 10 % 2 == 0;
+// 
+// // AND and OR Operator
+// var isLargerThan4andSmallerThan9 = number > 4 && number < 9;
+// var isEqualTo2OrLargerThan6OrSmallerThan200 = number == 6 || number > 6 || number < 200;
+// var isEqualTo123OrEvenAndSmallerThan20 = number == 123 || (number % 2 == 0 && number < 20);
+// 
+// // IF / ELSE Statements
+// if (userChoice.Equals("ABC"))
+// {
+//     Console.WriteLine("User typed ABC");
+// }
+// else
+// {
+//     Console.WriteLine("Nope");
+// }
+// 
 
-// Boolean and logical operation
-bool someBoolean = true;
-var someOtherBoolean = false;
-bool isUserInputAbc = userChoice == "ABC";
-bool isUserInputNotAbc = userChoice != "ABC";
-bool isUserInputNotAbc2 = !(userChoice == "ABC");
-
-var number = 10;
-var isLargerThan5 = number > 5;
-var isSmallerThan10 = number < 10;
-var isLargerOrEqualTo10 = number >= 10;
-var isSmallerOrEqualTo6 = number <= 6;
-
-var is10Modulo3EqualTo1 = 10 % 3 == 1;
-var isEven = 10 % 2 == 0;
-
-// AND and OR Operator
-var isLargerThan4andSmallerThan9 = number > 4 && number < 9;
-var isEqualTo2OrLargerThan6OrSmallerThan200 = number == 6 || number > 6 || number < 200;
-var isEqualTo123OrEvenAndSmallerThan20 = number == 123 || (number % 2 == 0 && number < 20);
-
-// IF / ELSE Statements
-if (userChoice.Equals("ABC"))
+if (userChoice.Equals("S"))
 {
-    Console.WriteLine("User typed ABC");
+    PrintSelectedOption("See all TODOs");
 }
-else
+else if (userChoice.Equals("A"))
 {
-    Console.WriteLine("Nope");
+    PrintSelectedOption("Add a TODO");
+}
+else if (userChoice.Equals("R"))
+{
+    PrintSelectedOption("Remove a TODO");
+}
+else if (userChoice.Equals("E"))
+{
+    PrintSelectedOption("Exit");
+}
+
+
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine($"Selected option: {selectedOption}");
 }
