b56bacbd115d9716cfdfa96c094df0acbe604efb
0xCrypt0nic <n.greneche45@gmail.com>
2025-12-07 11:59:28 +0100

    Boolean and logical operation

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -6,7 +6,24 @@ Console.WriteLine("[E]xit");
 
 string userChoice = Console.ReadLine();
 Console.WriteLine($"User input: {userChoice}");
+
 //todo: handle user's input.
 /*
 This is a multiple line comment.
 */
+
+// Boolean and logical operation
+bool someBoolean = true;
+var someOtherBoolean = false;
+bool isUserInputAbc = userChoice == "ABC";
+bool isUserInputNotAbc = userChoice != "ABC";
+bool isUserInputNotAbc2 = !(userChoice == "ABC");
+
+var number = 10;
+var isLargerThan5 = number > 5;
+var isSmallerThan10 = number < 10;
+var isLargerOrEqualTo10 = number >= 10;
+var isSmallerOrEqualTo6 = number <= 6;
+
+var is10Modulo3EqualTo1 = 10 % 3 == 1;
+var isEven = 10 % 2 == 0;
