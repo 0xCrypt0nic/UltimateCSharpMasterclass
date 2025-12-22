8a7c157d4a7d76847de6886fb9a02f08b1209f13
0xCrypt0nic <n.greneche45@gmail.com>
2025-12-22 06:35:04 +0100

    POO Concepts

---
+++ ToDoList/Learning/Program.cs
@@ -0,0 +1,17 @@
+﻿Rectangle rectangle1 = new Rectangle(5, 10);
+Console.WriteLine($"Rectangle infos : width {rectangle1.width}, height {rectangle1.height}");
+
+
+Console.ReadKey();
+
+class Rectangle
+{
+    public int width;
+    public int height;
+
+    public Rectangle(int _width, int _height)
+    {
+        width = _width;
+        height = _height;
+    }
+}
